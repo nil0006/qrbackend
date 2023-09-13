78388ec7eadcb65752263e40b284cae7f25b87de
nil0006 <niladrikarmakara006@gmail.com>
2023-09-13 20:06:41 +0530

    schema change

--- assets.js
+++ assets.js
@@ -3,6 +3,8 @@ const conn = require("./db").conn;
 const Schema = new mongoose.Schema({
     "assetId": Number,
     "stickerId": String,
+    "isActive": Boolean,
+    "date": Number
     
 });
 module.exports = conn.model("assets", Schema);
