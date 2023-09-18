aa79edbfe1d4d6763eebd4130e296fee2816bacb
nil0006 <niladrikarmakara006@gmail.com>
2023-09-18 10:53:20 +0530

    schema updated

--- assets.js
+++ assets.js
@@ -4,7 +4,12 @@ const Schema = new mongoose.Schema({
     "assetId": Number,
     "stickerId": String,
     "isActive": Boolean,
-    "date": String
-    
+    "allocatedRoomId": String,
+    "currentRoomId": String,
+    "allocatedUserId": String,
+    "assetModelName": String,
+    "assetModelId": String,
+    "buildingId": String,
+    "allocated": Boolean,
 });
 module.exports = conn.model("assets", Schema);
